8d397a997c632fc23481bb8dfa4775c4cbb444be
Abhay Naik <abhay.naik.20.1@mountblue.tech>
2022-06-28 11:40:30 +0530

    indent

--- src/server/q2Serve.js
+++ src/server/q2Serve.js
@@ -1,36 +1,39 @@
-const yearMatchesWon =getCSV2();
+const yearMatchesWon = getCSV2();
 
 async function getCSV2() {
-const getData = await fetch('http://127.0.0.1:5500/iplGraph/src/data/matches.csv');
-const dataMatches = await getData.text();
+  const getData = await fetch(
+    "http://127.0.0.1:5500/iplGraph/src/data/matches.csv"
+  );
+  const dataMatches = await getData.text();
 
-const rows =dataMatches.split('\n').slice(1).map((row)=> {
-   let finalRow= row.split(',');
-return finalRow;
-});
+  const rows = dataMatches
+    .split("\n")
+    .slice(1)
+    .map((row) => {
+      let finalRow = row.split(",");
+      return finalRow;
+    });
 
+  const yearMatchesWon = rows.reduce((noWon, eachMatch) => {
+    let year = eachMatch[1];
 
-
-const yearMatchesWon =rows.reduce((noWon,eachMatch)=>{
-        let year = eachMatch[1];
-        
-        let winnerTeam=eachMatch[10];
-        if(winnerTeam==""){}else{
-        if(noWon[winnerTeam]){
-            if(noWon[winnerTeam][year]){noWon[winnerTeam][year]+=1;}
-            else{noWon[winnerTeam][year]=1}
-            }
-        else{noWon[winnerTeam]={}
-            noWon[winnerTeam][year]=1}
+    let winnerTeam = eachMatch[10];
+    if (winnerTeam == "") {
+    } else {
+      if (noWon[winnerTeam]) {
+        if (noWon[winnerTeam][year]) {
+          noWon[winnerTeam][year] += 1;
+        } else {
+          noWon[winnerTeam][year] = 1;
         }
-        return noWon;
-        
-        },{})
+      } else {
+        noWon[winnerTeam] = {};
+        noWon[winnerTeam][year] = 1;
+      }
+    }
+    return noWon;
+  }, {});
 
-       
-    
-
-
-return yearMatchesWon;
+  return yearMatchesWon;
 }
-export{getCSV2}
+export { getCSV2 };
